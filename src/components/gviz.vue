cde64b53c096560c1805b3e36506345b4a6cb226
nk <nk@pop-os.localdomain>
2021-12-23 22:32:19 -0600

    use the vue-d3-network lib so we can actually make something instead of making the network viz for two weeks.

--- src/components/gviz.vue
+++ src/components/gviz.vue
@@ -1,50 +1,24 @@
 <template>
   <div id="gviz">
-    {{graph.nodes}}
     <button @click="testMessage()" right>test me</button>
-    <svg :width="width+'px'"
-        :height="height+'px'"
-        @mousemove="drag($event)"
-        @mouseup="drop()"
-        v-if="bounds.minX">
-      <line v-for="(link, linkIndex) in graph.links" :key="linkIndex"
-        :x1="coords[link.source.index].x"
-        :y1="coords[link.source.index].y"
-        :x2="coords[link.target.index].x"
-        :y2="coords[link.target.index].y"
-        stroke="black" stroke-width="2"/>
-      <circle v-for="(node, nodeIndex) in graph.nodes" :key="nodeIndex"
-        :cx="coords[nodeIndex].x"
-        :cy="coords[nodeIndex].y"
-        :r="20"
-        :fill="colors[Math.ceil(Math.sqrt(node.index))]"
-        stroke="white"
-        stroke-width="1"
-        @mousedown="currentMove = {x: $event.screenX, y: $event.screenY, node: node}"
-      >
-      </circle>
-      <foreignObject v-for="(node, nodeIndex) in graph.nodes" :key="nodeIndex"
-        :x="coords[nodeIndex].x - 150"
-        :y="coords[nodeIndex].y - 60"
-        :width="300"
-        :height="35"
-        :fill="colors[Math.ceil(Math.sqrt(node.index))]"
-      >
-        <div style="color: white; font: 12px serif; height: 100%; overflow: auto; text-align: center">
-          <b>id</b>:{{node.id}}
-          <br>
-          <b>label</b>: {{node.label}}
-        </div>
-      </foreignObject>
-    </svg>
+    <d3-network
+      v-if="nodes"
+      :net-nodes="nodes"
+      :net-links="links"
+      :options="options"
+    />
   </div>
 </template>
 
 <script>
-import * as d3 from "d3"
+// import * as d3 from "d3"
+import D3Network from 'vue-d3-network'
 export default {
   name: "gviz",
   props: {},
+  components: {
+    "d3-network": D3Network
+  },
   created() {
     // this.updateSimulation()
 
@@ -56,51 +30,31 @@ export default {
       console.log(error)
     }
     this.gremlin_websocket.onmessage = (event) => {
-      var vertices = JSON.parse(JSON.stringify(this.parseGraphson(JSON.parse(event.data).result.data)))
-      this.graph.nodes = vertices
-      this.graph.links = []
+      this.nodes = JSON.parse(JSON.stringify(this.parseGraphson(JSON.parse(event.data).result.data)))
+      this.links = [
+        { 
+          id: "1bcb9911-bbbb-aaaa-cccc-1ab8e09f8b8c",
+          name: "knows",
+          tid: '1bcb9911-b7f9-4916-914b-1ab8e09f8b8c',
+          sid: '42e7fe6d-b67f-42b1-a99d-27e1949519f1',
+          _color: "black"
+        }
+        ]
       this.updateSimulation()
     }
-    // var query = "g.V().elementMap().toList()"
-    // var message = JSON.stringify(this.createMessage(query))
-    // this.gremlin_websocket.send(message)
-    // console.log(this.gremlin_websocket)
   },
   methods: {
-    drag(e) {
-      if (this.currentMove) {
-        this.currentMove.node.fx = this.currentMove.node.x - (this.currentMove.x - e.screenX) * (this.bounds.maxX - this.bounds.minX) / (this.width - 2 * this.padding)
-        this.currentMove.node.fy = this.currentMove.node.y -(this.currentMove.y - e.screenY) * (this.bounds.maxY - this.bounds.minY) / (this.height - 2 * this.padding)
-        this.currentMove.x = e.screenX
-        this.currentMove.y = e.screenY
-      }
-    },
-    drop(){
-      if (this.currentMove) {
-        delete this.currentMove.node.fx
-        delete this.currentMove.node.fy    
-        this.currentMove = null
-        this.simulation.alpha(1)
-        this.simulation.restart()
-      }
-    },
     parseGraphson(graphson) {
       return graphson["@value"].map(i => { 
-        return { 
+        return {
           id: i["@value"].id["@value"],
           label: i["@value"].label,
+          name: "ID: " + i["@value"].id["@value"] + " LABEL: " + i["@value"].label,
+          _color: "green",
           x: null,
           y: null,
-        } 
+        }
       })
-    },
-    updateSimulation() {
-      this.simulation = d3.forceSimulation(this.graph.nodes)
-        .force('center', d3.forceCenter(this.width / 2, this.height / 2))
-        .force('collision', d3.forceCollide().radius(10))
-        .force('link', d3.forceLink(this.graph.links))
-        // .force('x', d3.forceX().x(this.width / 2))
-        // .force('y', d3.forceY().y(this.height / 2))
     },
     testMessage() {
       var query = "g.V()"
@@ -121,66 +75,24 @@ export default {
     },
   },
   computed: {
-    bounds() {
-      return {
-        minX: Math.min(...this.graph.nodes.map(n => n.x)),
-        maxX: Math.max(...this.graph.nodes.map(n => n.x)),
-        minY: Math.min(...this.graph.nodes.map(n => n.y)),
-        maxY: Math.max(...this.graph.nodes.map(n => n.y))
-      }
-    },
-    coords() {
-      return this.graph.nodes.map(node => {
-        return {
-          x: this.padding + (node.x - this.bounds.minX) * (this.width - 2*this.padding) / (this.bounds.maxX - this.bounds.minX),
-          y: this.padding + (node.y - this.bounds.minY) * (this.height - 2*this.padding) / (this.bounds.maxY - this.bounds.minY)
-        }
-      })
-    }
   },
   data() {
     return {
       gremlin_websocket: null,
-      graph: {
-        nodes: d3.range(100).map(i => ({ index: i, x: null, y: null })),
-        links: d3.range(100).map(i => ({ source: i, target: Math.floor(Math.random() * i) }))
-      },
+      nodes: [],
+      links: [],
       width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
       height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 40,
-      padding: 100,
-      colors: ['#2196F3', '#E91E63', '#7E57C2', '#009688', '#00BCD4', '#EF6C00', '#4CAF50', '#FF9800', '#F44336', '#CDDC39', '#9C27B0'],
-      simulation: null,
-      currentMove: null,
+      options: {
+        nodeSize: 35,
+        nodeLabels: true,
+        linkLabels: true,
+        force: 10000
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-/* Tooltip container */
-.tooltip {
-  position: relative;
-  display: inline-block;
-  border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
-}
-
-/* Tooltip text */
-.tooltip .tooltiptext {
-  visibility: hidden;
-  width: 120px;
-  background-color: black;
-  color: #fff;
-  text-align: center;
-  padding: 5px 0;
-  border-radius: 6px;
- 
-  /* Position the tooltip text - see examples below! */
-  position: absolute;
-  z-index: 1;
-}
-
-/* Show the tooltip text when you mouse over the tooltip container */
-.tooltip:hover .tooltiptext {
-  visibility: visible;
-}
 </style>
--- tinkergraphDevServer/testVertices.py
+++ tinkergraphDevServer/testVertices.py
@@ -6,7 +6,11 @@ url = 'ws://localhost:8182/gremlin'
 remoteConn = DriverRemoteConnection(url, 'g')
 
 g = traversal().withRemote(remoteConn)
-g.V('0abe3186-e763-4b4c-a3ed-dfcdf5d8e1b9').addE('knows').to(__.V('aa24c147-68f9-41ee-8099-21a6bd87b9dc')).next()
+
+g.addV('person').properties('name', 'paul').iterate()
+g.addV('person').properties('name', 'fred').iterate()
+g.addV('person').properties('name', 'tim').iterate()
+
 results = g.V().elementMap().toList()
 for result in results:
     print(result)
